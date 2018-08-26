0a9f7916bb0b725e17cc1447af8c129077e94fe6
Aviram Adiri <aviram.adiri@hotmail.com>
2018-08-27 00:28:50 +0300

    using the serverMock, and fixing the card to fit Teams

--- Controllers/MessagesController.cs
+++ Controllers/MessagesController.cs
@@ -7,12 +7,18 @@ using System.Web.Http.Description;
 using System.Net.Http;
 using SimpleEchoBot.Dialogs;
 using LogicLayer;
+using System.Collections.Generic;
+using System;
+using System.Text;
+using EntitiesLayer;
+using System.Linq;
 
 namespace Microsoft.Bot.Sample.SimpleEchoBot
 {
     [BotAuthentication]
     public class MessagesController : ApiController
     {
+        private bool isTeams = true;
         /// <summary>
         /// POST: api/Messages
         /// receive a message from a user and send replies
@@ -21,17 +27,224 @@ namespace Microsoft.Bot.Sample.SimpleEchoBot
         [ResponseType(typeof(void))]
         public virtual async Task<HttpResponseMessage> Post([FromBody] Activity activity)
         {
-            var x = ServerMock.GetInstance();
+            ConnectorClient connector = new ConnectorClient(new System.Uri(activity.ServiceUrl));
+
             // check if activity is of type message
             if (activity != null && activity.GetActivityType() == ActivityTypes.Message)
             {
-                await Conversation.SendAsync(activity, () => new AI_Dialog());
+
+                Activity replyToConversation = CreateReply(activity);
+
+                await connector.Conversations.SendToConversationAsync(replyToConversation);
+                //await Conversation.SendAsync(activity, () => new AI_Dialog());
             }
             else
             {
                 HandleSystemMessage(activity);
             }
             return new HttpResponseMessage(System.Net.HttpStatusCode.Accepted);
+        }
+
+        private Activity CreateReply(Activity activity)
+        {
+            Activity replyToConversation;
+            if (string.Equals(activity.Text, UserMessage.FirstMessage, StringComparison.OrdinalIgnoreCase)) {
+                replyToConversation = activity.CreateReply("Hi there :)");
+                Attachment attachment = CreateFirstCard();
+                replyToConversation.Attachments.Add(attachment);
+            }
+            else if (string.Equals(activity.Text, UserMessage.SecondMessage, StringComparison.OrdinalIgnoreCase))
+            {
+                replyToConversation = activity.CreateReply("ok...");
+                Attachment attachment = CreateSecondCard();
+                replyToConversation.Attachments.Add(attachment);
+            }
+            else if (string.Equals(activity.Text, UserMessage.ThirdMessage, StringComparison.OrdinalIgnoreCase))
+            {
+                replyToConversation = activity.CreateReply("Hi again!");
+                Attachment attachment = CreateThirdCard();
+                replyToConversation.Attachments.Add(attachment);
+            }
+            else if (string.Equals(activity.Text, UserMessage.FourthMessage, StringComparison.OrdinalIgnoreCase))
+            {
+                replyToConversation = activity.CreateReply("I see the meeting is ended");
+                Attachment attachment = CreateFourtCard();
+                replyToConversation.Attachments.Add(attachment);
+            }
+            else
+            {
+                replyToConversation = activity.CreateReply(UserMessage.CrateHelpMessage());
+            }
+            return replyToConversation;
+        }
+
+        private Attachment CreateFirstCard()
+        {
+            var server = ServerMock.GetInstance();
+            var myContact = server.GetContactByName("shir esh");
+
+            Meeting meeting = server.FindNextMeeting(myContact);
+            Contact otherContact = meeting.Attendees.Where(c => c != myContact).FirstOrDefault();
+            var tips = server.GetTipsAboutPersonForMeeting(meeting, myContact, false);
+            List<string> tipContents = tips.Select(x => x.Content).ToList();
+
+            string concatenedTips = ConcatAllTips(tipContents);
+
+            var card = new HeroCard //ThumbnailCard
+            {
+                Title = $"Your next meeting is with {otherContact.Name}",
+                Subtitle = $"Meeting title: {meeting.Title}",
+                Text = concatenedTips,
+                Images = new List<CardImage>()
+                {
+                    new CardImage(otherContact.ImagePath)
+                },
+                Buttons = new List<CardAction>()
+                {
+                    new CardAction("openUrl", "Linkedin", value: otherContact.ImagePath)
+                }
+            };
+            
+            // Create the attachment.
+            Attachment attachment = new Attachment()
+            {
+                ContentType = HeroCard.ContentType,
+                Content = card
+            };
+            return attachment;
+        }
+
+        // TODO: change this card!
+        private Attachment CreateSecondCard()
+        {
+            var server = ServerMock.GetInstance();
+            var myContact = server.GetContactByName("shir esh");
+
+            Meeting meeting = server.FindNextMeeting(myContact);
+            Contact otherContact = meeting.Attendees.Where(c => c != myContact).FirstOrDefault();
+            var tips = server.GetTipsAboutPersonForMeeting(meeting, myContact, false);
+            List<string> tipContents = tips.Select(x => x.Content).ToList();
+
+            string concatenedTips = ConcatAllTips(tipContents);
+
+            var card = new HeroCard //ThumbnailCard
+            {
+                Title = $"Your next meeting is with {otherContact.Name}",
+                Subtitle = $"Meeting title: {meeting.Title}",
+                Text = concatenedTips,
+                Images = new List<CardImage>()
+                {
+                    new CardImage(otherContact.ImagePath)
+                },
+                Buttons = new List<CardAction>()
+                {
+                    new CardAction("openUrl", "Linkedin", value: otherContact.ImagePath)
+                }
+            };
+
+            // Create the attachment.
+            Attachment attachment = new Attachment()
+            {
+                ContentType = HeroCard.ContentType,
+                Content = card
+            };
+            return attachment;
+        }
+
+        // TODO: change this card!
+        private Attachment CreateThirdCard()
+        {
+            var server = ServerMock.GetInstance();
+            var myContact = server.GetContactByName("shir esh");
+
+            Meeting meeting = server.FindNextMeeting(myContact);
+            Contact otherContact = meeting.Attendees.Where(c => c != myContact).FirstOrDefault();
+            var tips = server.GetTipsAboutPersonForMeeting(meeting, myContact, false);
+            List<string> tipContents = tips.Select(x => x.Content).ToList();
+
+            string concatenedTips = ConcatAllTips(tipContents);
+
+            var card = new HeroCard //ThumbnailCard
+            {
+                Title = $"Your next meeting is with {otherContact.Name}",
+                Subtitle = $"Meeting title: {meeting.Title}",
+                Text = concatenedTips,
+                Images = new List<CardImage>()
+                {
+                    new CardImage(otherContact.ImagePath)
+                },
+                Buttons = new List<CardAction>()
+                {
+                    new CardAction("openUrl", "Linkedin", value: otherContact.ImagePath)
+                }
+            };
+
+            // Create the attachment.
+            Attachment attachment = new Attachment()
+            {
+                ContentType = HeroCard.ContentType,
+                Content = card
+            };
+            return attachment;
+        }
+
+        // TODO: change this card!
+        private Attachment CreateFourtCard()
+        {
+            var server = ServerMock.GetInstance();
+            var myContact = server.GetContactByName("shir esh");
+
+            Meeting meeting = server.FindNextMeeting(myContact);
+            Contact otherContact = meeting.Attendees.Where(c => c != myContact).FirstOrDefault();
+            var tips = server.GetTipsAboutPersonForMeeting(meeting, myContact, false);
+            List<string> tipContents = tips.Select(x => x.Content).ToList();
+
+            string concatenedTips = ConcatAllTips(tipContents);
+
+            var card = new HeroCard //ThumbnailCard
+            {
+                Title = $"Your next meeting is with {otherContact.Name}",
+                Subtitle = $"Meeting title: {meeting.Title}",
+                Text = concatenedTips,
+                Images = new List<CardImage>()
+                {
+                    new CardImage(otherContact.ImagePath)
+                },
+                Buttons = new List<CardAction>()
+                {
+                    new CardAction("openUrl", "Linkedin", value: otherContact.ImagePath)
+                }
+            };
+
+            // Create the attachment.
+            Attachment attachment = new Attachment()
+            {
+                ContentType = HeroCard.ContentType,
+                Content = card
+            };
+            return attachment;
+        }
+
+        private string NewLine()
+        {
+            if (isTeams)
+            {
+                return " <br> ";
+            }
+
+            return " \n\r ";
+        }
+
+        private string ConcatAllTips(List<string> tips)
+        {
+            int i = 1;
+            var ans = new StringBuilder("Meeting Tips:");
+            foreach (string tip in tips)
+            {
+                ans = ans.Append(NewLine()).Append($"   {i}) ").Append(tip);
+                i++;
+            }
+            return ans.ToString();
         }
 
         private Activity HandleSystemMessage(Activity message)
@@ -61,6 +274,25 @@ namespace Microsoft.Bot.Sample.SimpleEchoBot
             }
 
             return null;
+        }
+    }
+
+    public static class UserMessage
+    {
+        public static string FirstMessage = "my next meeting";
+        public static string SecondMessage = "next tips";
+        public static string ThirdMessage = "next immediate meeting";
+        public static string FourthMessage = "rate tips";
+
+        public static string CrateHelpMessage()
+        {
+            string ans = "I'm sorry, I just understand the following requests:";
+            ans += "\n - " + FirstMessage;
+            ans += "\n - " + SecondMessage;
+            ans += "\n - " + ThirdMessage;
+            ans += "\n - " + FourthMessage;
+            return ans;
+
         }
     }
 }
---
+++ ServerTests/Program.cs
@@ -0,0 +1,26 @@
+﻿using EntitiesLayer;
+using LogicLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var server = ServerMock.GetInstance();
+            Contact curContact = server.GetContactByName("shir esh");
+            string name = curContact.Name;
+            string image = curContact.ImagePath;
+            string linkedin = curContact.LinkedinPath;
+
+            Meeting meeting = server.FindNextMeeting(curContact);
+            string title = meeting.Title;
+            Console.Read();
+        }
+    }
+}
